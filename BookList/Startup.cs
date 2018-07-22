a9176c4e2f81741c89702b96486616bf5c717437
Ian @ home <ian@visdata.co.uk>
2018-07-22 21:42:17 +0100

    Done

--- BookList/Startup.cs
+++ BookList/Startup.cs
@@ -24,7 +24,7 @@ namespace BookList
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("LaptopConnection")));
+                options.UseSqlServer(Configuration.GetConnectionString("DesktopConnection")));
             services.AddMvc();
         }
 
